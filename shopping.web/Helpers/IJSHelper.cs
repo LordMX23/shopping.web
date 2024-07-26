4498e5dcdbcb8621f5df697343a4cd83a4c4084c
LordMX23 <jossue_08@hotmail.com>
2024-07-26 14:43:10 -0600

    actualiza

---
+++ shopping.web/Helpers/IJSHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+
+namespace shopping.web.Helpers
+{
+    public static class IJSHelper
+    {
+        // Metodo de extension
+        public static async ValueTask ToastrSuccess(this IJSRuntime JsRuntime, string message)
+        {
+            await JsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+
+        public static async ValueTask ToastrError(this IJSRuntime JsRuntime, string message)
+        {
+            await JsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+    }
+}
